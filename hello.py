ae8ea0163ac3f786a19224befccfbcd93c7a069d
soojin66 <rlatnwls0606@naver.com>
2021-05-12 14:58:46 +0900

    add Git fun

--- hello.py
+++ hello.py
@@ -5,7 +5,11 @@ def fun_2():
         print(" World")
         
 def fun_3():
-        print(" Github")
+    print(" Github")
+
+def fun_4():
+        print(" Git")
 fun_1()
 fun_2()
 fun_3()
+fun_4()
